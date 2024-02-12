c19c2950f7f5b7fedfa9561c8791f1fb3224a651
shantanubose01 <sbose1918@gmail.com>
2024-02-13 00:36:31 +0530

    fix: added near lift strategy

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-// MADE BY SHANTANU BOSE
+// MADE BY SHANTANU
 
 const noOfLifts = document.querySelector("#lifts");
 const noOfFloors = document.querySelector("#floors");
 const submitBtn = document.querySelector("#submit-btn");
 const inputForm = document.querySelector(".input-form");
-let i = -1;
-
+// let i = -1;
 
 const generateFloor = (floorNo) => {
   const newFloor = document.createElement("div");
@@ -15,6 +14,7 @@ const generateFloor = (floorNo) => {
   newFloor.className = "floor";
   newFloor.id = `floor-${floorNo}`;
   newFloor.textContent = `Floor ${floorNo}`;
+  newFloor.setAttribute("data-floor", floorNo);
 
   upBtn.textContent = "Up";
   downBtn.textContent = "Down";
@@ -26,17 +26,17 @@ const generateFloor = (floorNo) => {
   floorBtnContainer.append(upBtn, downBtn);
   floorBtnContainer.style.position = "absolute";
   floorBtnContainer.style.left = "70px";
-  upBtn.addEventListener("click", () => moveLift(newFloor.id));
-  downBtn.addEventListener("click", () => moveLift(newFloor.id));
+  upBtn.addEventListener("click", () => moveLift("btn-up", floorNo));
+  downBtn.addEventListener("click", () => moveLift("btn-down", floorNo));
   newFloor.appendChild(floorBtnContainer);
   document.getElementById("container").append(newFloor);
 };
-
 
 const generateLift = (liftNo) => {
   const newLift = document.createElement("div");
   newLift.className = "lift";
   newLift.id = `lift-${liftNo}`;
+  newLift.setAttribute("data-floor", "0");
 
   document.getElementById("container").lastElementChild.appendChild(newLift);
 
@@ -49,32 +49,31 @@ const generateLift = (liftNo) => {
   newLift.append(leftDoor, rightDoor);
 };
 
-
-const moveLift = (floorId) => {
-  const floorNo = floorId.split("-");
-  const liftId = liftSelector();
-  const lift = document.getElementById(liftId);
-
-  // Calculate the distance to move the lift. The floor is 100px height each.
-  const distance = (Number(floorNo[1]) - 1) * 100;
-
-  // Calculate the number of floors to move the lift.
-  const totalFloorsToMove = distance / 100;
-
-  // Calculate the duration for the lift to move 2s per floor.
-  const duration = 2000 * totalFloorsToMove;
-
-  // Move the lift using CSS transition
-  lift.style.transition = `transform ${duration}ms ease-in-out`;
-  lift.style.transform = `translateY(-${distance}px)`;
-
-  // Wait for the transition to end
-  lift.addEventListener("transitionend", () => {
-    openDoors(liftId);
-  });
+const moveLift = (id, floorClicked) => {
+  
+  const lifts = Array.from(document.querySelectorAll(".lift"));
+  for (let i = 0; i < lifts.length; i++) {
+    const lift = lifts[i];
+    const currentFloor = +lift.dataset.floor;
+    const { height } = document
+      .querySelector(`[data-floor='${floorClicked}']`)
+      .getBoundingClientRect();
+    const floorCondition =
+      id === "btn-down"
+        ? currentFloor > floorClicked
+        : currentFloor < floorClicked;
+    if (floorCondition && currentFloor !== floorClicked) {
+      lift.style.transform = `translateY(-${height * floorClicked}px)`;
+      lift.style.transition = `all ${floorClicked + 1}s ease-in`;
+      lift.dataset.floor = floorClicked;
+      lift.addEventListener("transitionend", function () {
+        openDoors(lift.id);
+      });
+    
+      break;
+    }
+  }
 };
-
-
 
 const openDoors = (liftId) => {
   document.getElementById(liftId).classList.add("open");
@@ -85,12 +84,9 @@ const openDoors = (liftId) => {
   });
 };
 
-
 const closeDoors = (liftId) => {
   document.getElementById(liftId).classList.remove("open");
 };
-
-
 
 const liftSelector = () => {
   const allLifts = document.querySelectorAll(".lift");
@@ -109,15 +105,12 @@ const liftSelector = () => {
   }
 };
 
-
 const generateBtn = (text) => {
   const btn = document.createElement("button");
   btn.textContent = text;
   btn.classList.add("btn", "generate-btn", "bold-600");
   return btn;
 };
-
-
 
 const resetBtnHandler = () => {
   const resetBtn = generateBtn("Reset");
@@ -132,7 +125,6 @@ const resetBtnHandler = () => {
 
   return resetBtn;
 };
-
 
 const restartBtnHandler = () => {
   const restartBtn = generateBtn("Restart");
@@ -150,7 +142,6 @@ const restartBtnHandler = () => {
   return restartBtn;
 };
 
-
 submitBtn.addEventListener("click", function (event) {
   // to prevent default behaviour.
   event.preventDefault();
@@ -163,7 +154,7 @@ submitBtn.addEventListener("click", function (event) {
     inputForm.style.display = "none";
 
     // This will generate each floor.
-    for (let i = +noOfFloors.value; i > 0; i--) {
+    for (let i = +noOfFloors.value - 1; i >= 0; i--) {
       generateFloor(i);
     }
 
@@ -175,8 +166,8 @@ submitBtn.addEventListener("click", function (event) {
     const allFloors = document.querySelectorAll(".floor");
 
     // display the first floor down btn and last floor up button to none.
-    allFloors[0].querySelector(".up-btn").style.display = "none";
-    allFloors[allFloors.length - 1].querySelector(".down-btn").style.display =
+    allFloors[0].querySelector(".down-btn").style.display = "none";
+    allFloors[allFloors.length - 1].querySelector(".up-btn").style.display =
       "none";
 
     // This will reset the game and bring back lifts to the first floor.
@@ -193,3 +184,4 @@ submitBtn.addEventListener("click", function (event) {
     btnContainer.style.textAlign = "center";
   }
 });
+
